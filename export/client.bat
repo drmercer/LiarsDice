0bfae60e0a01408364dfdf15220b5d99585811f7
Dan Mercer <danielmercer1@gmail.com>
2013-08-11 15:36:23 -0600

    Added launcher batch files

---
+++ export/client.bat
@@ -0,0 +1,5 @@
+@echo off
+title Liar's Dice by drmercer
+java -jar program.jar --name "Dan" --join
+echo Exited.
+pause
---
+++ export/server.bat
@@ -0,0 +1,5 @@
+@echo off
+title Liar's Dice by drmercer
+java -jar program.jar --dice-per-player 5 --name "Dan"
+echo Exited.
+pause
